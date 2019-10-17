c99d3e5981aa065012b1a24b3fa86658b1f2adc0
RSharma <rochak.sharma@davita.com>
2019-10-17 13:43:08 -0500

    Add project files.

---
+++ POCSampleWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace POCSampleWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
